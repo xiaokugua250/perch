0487126bda96c113ec47dcb295a79cc24158a8a9
duliang <liang.du@nscc-gz.cn>
2020-07-11 17:02:34 +0800

    1、初始化添加爬虫信息

--- internal/spider/crawl/crawl.go
+++ internal/spider/crawl/crawl.go
@@ -1,5 +1,35 @@
 package crawl
 
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
 type SpiderCrawer interface {
-	CrawlerFetch()
+	CrawlerFetch(url string) error
+}
+
+func CrawlerFetch(url string, method string, depth int, requestData io.Reader, ctx *context.Context, requestHeader http.Header, checkRevisited bool) error {
+	var err error
+	httpClient := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("request url %s,response is %s\n", url, string(body))
+
+	return nil
 }
