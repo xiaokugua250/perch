ae9155c59fdbb9c1121cc181a97fe1ddb5b60355
duliang <liang.du@nscc-gz.cn>
2020-09-04 17:28:42 +0800

    添加psgoutils相关方法

---
+++ website/src/views/dashboard/admin/index.vue
@@ -0,0 +1,247 @@
+<template>
+  <div class="dashboard-editor-container">
+    <github-corner class="github-corner" />
+
+    <panel-group @handleSetLineChartData="handleSetLineChartData" />
+    <el-row :gutter="20">
+    <el-col :span="6">
+      <div class="chart-wrapper">
+
+        <div slot="header" class="clearfix">
+          <span>平台服务(PLATADMIN)</span>
+          <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+        </div>
+
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div class="chart-wrapper">
+
+        <div slot="header" class="clearfix">
+          <span>数据库服务(MySQL)</span>
+          <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+        </div>
+
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div class="chart-wrapper">
+
+        <div slot="header" class="clearfix">
+          <span>系统监控(LINUX)</span>
+          <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+        </div>
+
+      </div>
+    </el-col>
+    <el-col :span="6">       <div class="chart-wrapper">
+
+      <div slot="header" class="clearfix">
+        <span>爬虫服务(Colly)</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+
+      <!--   <el-card class="box-card">
+           <div slot="header" class="clearfix">
+             <span>卡片名称</span>
+             <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+           </div>
+           <div v-for="o in 4" :key="o" class="text item">
+             {{'列表内容 ' + o }}
+           </div>
+         </el-card>-->
+    </div></el-col>
+
+  </el-row>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="chart-wrapper">
+
+          <div slot="header" class="clearfix">
+            <span>复杂网络(NETWORK)</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="chart-wrapper">
+
+          <div slot="header" class="clearfix">
+            <span>容器与虚拟化(Vituralization)</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="chart-wrapper">
+
+          <div slot="header" class="clearfix">
+            <span>调度系统(K8S)</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+
+        </div>
+      </el-col>
+      <el-col :span="6">       <div class="chart-wrapper">
+
+        <div slot="header" class="clearfix">
+          <span>其他(OTHERS)</span>
+          <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+        </div>
+
+        <!--   <el-card class="box-card">
+             <div slot="header" class="clearfix">
+               <span>卡片名称</span>
+               <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+             </div>
+             <div v-for="o in 4" :key="o" class="text item">
+               {{'列表内容 ' + o }}
+             </div>
+           </el-card>-->
+      </div></el-col>
+
+    </el-row>
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <line-chart :chart-data="lineChartData" />
+    </el-row>
+
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <raddar-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <pie-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <bar-chart />
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="8">
+      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
+        <transaction-table />
+      </el-col>
+      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+        <todo-list />
+      </el-col>
+      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+        <box-card />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import GithubCorner from '@/components/GithubCorner'
+import PanelGroup from './components/PanelGroup'
+import LineChart from './components/LineChart'
+import RaddarChart from './components/RaddarChart'
+import PieChart from './components/PieChart'
+import BarChart from './components/BarChart'
+import TransactionTable from './components/TransactionTable'
+import TodoList from './components/TodoList'
+import BoxCard from './components/BoxCard'
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
+export default {
+  name: 'DashboardAdmin',
+  components: {
+    GithubCorner,
+    PanelGroup,
+    LineChart,
+    RaddarChart,
+    PieChart,
+    BarChart,
+    TransactionTable,
+    TodoList,
+    BoxCard
+  },
+  data() {
+    return {
+      lineChartData: lineChartData.newVisitis
+    }
+  },
+  methods: {
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .github-corner {
+    position: absolute;
+    top: 0px;
+    border: 0;
+    right: 0;
+  }
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+@media (max-width:1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
+</style>
