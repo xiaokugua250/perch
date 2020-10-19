7ee6e63356ce7d7ebbdeb67465fe76cde4618fa9
duliang <liang.du@nscc-gz.cn>
2020-10-19 10:36:30 +0800

    1,单例模式初始化集群配置

--- pkg/cluster/k8s/k8s_client.go
+++ pkg/cluster/k8s/k8s_client.go
@@ -8,24 +8,75 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	_ "perch/pkg/log"
+	"sync"
 )
 
+var (
+	once                  sync.Once
+	clusterClientInstance *clusterClientManager
+)
+
+type clusterClientManager struct {
+	clusterClient *kubernetes.Clientset
+}
+
+//单例模式进行初始化,
+//注意通常情况下不需要前置参数，此种模式并不规范 https://stackoverflow.com/search?page=1&tab=Relevance&q=singleton%20with%20param
+func (k8sClusterManager *ClusterManager) NewClusterClientInstance() *clusterClientManager {
+
+	once.Do(
+		func() {
+			//config,err:= clientcmd.BuildConfigFromFlags("",os.Getenv("kubeconfig"))
+			config, err := clientcmd.BuildConfigFromFlags("", k8sClusterManager.KubeConfig.ConfigFile)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			clientset, err := kubernetes.NewForConfig(config)
+			if err != nil {
+				log.Fatalln(err)
+				clusterClientInstance = &clusterClientManager{clusterClient: clientset}
+			}
+		},
+	)
+	return clusterClientInstance
+}
+
+//单例模式进行初始化
+func NewClusterClientInstance() *clusterClientManager {
+
+	once.Do(
+		func() {
+			config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("kubeconfig"))
+			//config, err := clientcmd.BuildConfigFromFlags("", k8sClusterManager.KubeConfig.ConfigFile)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			clientset, err := kubernetes.NewForConfig(config)
+			if err != nil {
+				log.Fatalln(err)
+				clusterClientInstance = &clusterClientManager{clusterClient: clientset}
+			}
+		},
+	)
+	return clusterClientInstance
+}
 
 type ClusterManager struct {
-	KubeConfig      KUBEConfig`yaml:"kubeconfig"`
+	KubeConfig KUBEConfig `yaml:"kubeconfig"`
 }
 type KUBEConfig struct {
 	ClusterName string `yaml:"cluster_name"`
-	ConfigFile string `yaml:"config_file"`
+	ConfigFile  string `yaml:"config_file"`
 }
 
-type ClientSet struct {
+/*type ClientSet struct {
 	K8SClientSet *kubernetes.Clientset
-}
+}*/
 
 var (
 	ClusterClientMap = make(map[string]*kubernetes.Clientset)
-	K8SClientSet     = ClientSet{}
+
+//	K8SClientSet     = ClientSet{}
 )
 
 /**
@@ -46,32 +97,31 @@ func (k8sClusterManager *ClusterManager) InitK8SClusterClient() error {
 
 }
 
-
 /**
 初始化k8s集群
 */
-func  InitKubernetesCluster() error {
+func InitKubernetesCluster() error {
 	var (
-		k8sClusterManager  ClusterManager
-		err error
+		k8sClusterManager ClusterManager
+		err               error
 	)
 
 	RunEnv := os.Getenv("RUN_ENV")
-	if RunEnv == ""{
-		RunEnv="dev"
+	if RunEnv == "" {
+		RunEnv = "dev"
 	}
-//	viper.AddConfigPath("E:\\WorksSpaces\\GoWorkSpaces\\perch\\configs\\"+RunEnv+"\\cluster_config")
+	//	viper.AddConfigPath("E:\\WorksSpaces\\GoWorkSpaces\\perch\\configs\\"+RunEnv+"\\cluster_config")
 	viper.AddConfigPath("E:\\WorksSpaces\\GoWorkSpaces\\perch\\configs\\dev\\cluster_config")
 	viper.SetConfigName("kubernetes")
 	//viper.SetConfigFile("kubernetes_cluster.config")
 	err = viper.ReadInConfig()
-	if err!= nil{
+	if err != nil {
 		return err
 	}
-	err =viper.Unmarshal(&k8sClusterManager, func(config *mapstructure.DecoderConfig) {
-		config.TagName="yaml"
+	err = viper.Unmarshal(&k8sClusterManager, func(config *mapstructure.DecoderConfig) {
+		config.TagName = "yaml"
 	})
-	if err!= nil{
+	if err != nil {
 		return err
 	}
 	config, err := clientcmd.BuildConfigFromFlags("", k8sClusterManager.KubeConfig.ConfigFile)
@@ -83,7 +133,7 @@ func  InitKubernetesCluster() error {
 		log.Fatalln(err)
 	}
 	ClusterClientMap[k8sClusterManager.KubeConfig.ClusterName] = clientset
-	K8SClientSet.K8SClientSet=clientset
+	//K8SClientSet.K8SClientSet = clientset
 	return nil
 
 }
