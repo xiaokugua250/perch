20b2549baf4a773b44f4409829f36c1594b4d7e6
xiaokugua250 <xiaokugua250@hotmail.com>
2020-12-01 22:02:33 +0800

    1,修改解析器

--- internal/spider/anaysis/website_anaysis/github/projects_information_test.go
+++ internal/spider/anaysis/website_anaysis/github/projects_information_test.go
@@ -18,16 +18,31 @@ func TestBaseProjectInformations(t *testing.T) {
 	doc, err := goquery.NewDocumentFromReader(bufio.NewReader(file))
 	Readme := doc.Find("div#readme")
 	//fmt.Printf("readme text is %s\n",Readme.Text())
+	targetFile, err := os.OpenFile("github_projects.txt", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Errorln(err)
+	}
+	//	defer target_file.Close()
+	write := bufio.NewWriter(targetFile)
 	Readme.Find("li").Has("a").Children().Each(func(i int, selection *goquery.Selection) {
 		if i > 100 {
 			//selection.Each()
-			url, ok := selection.Attr("href")
-			if ok {
+			url, _ := selection.Attr("href")
 
+			str := fmt.Sprintf("project name is: %s, url is: %s, project description is: %s\n", selection.Text(), url, selection.Parent().Text())
+			//fmt.Printf("str is %s\n",str)
+			_, err := write.WriteString(str)
+			if err != nil {
+				log.Errorln(err)
 			}
-			fmt.Printf(" index is %d ,readme text is %s,url is %s ,descriptions is %s\n", i, selection.Text(), url, selection.Parent().Text())
+
+			//rr=ioutil.WriteFile("github_projects.txt",[]byte(fmt.Sprintf("project name is: %s, url is: %s, project description  is %s\n",selection.Text(), url, selection.Parent().Text())),os.ModeAppend|0644)
+
+			//fmt.Printf(" index is %d ,readme text is %s,url is %s ,descriptions is %s\n", i, )
 		}
+
 		//if selection.Children()
 	})
+	write.Flush()
 
 }
