d41aa6c82c50e430e2b3f2a154073ab4f350dbf7
duliang <liang.du@nscc-gz.cn>
2020-12-23 17:28:24 +0800

    　

---
+++ pkg/fake/fake.go
@@ -0,0 +1,99 @@
+// refer
+package fake
+
+import (
+	"fmt"
+	"github.com/icrowley/fake"
+	"strconv"
+	"time"
+)
+
+//var log = logrus.New()
+
+func GenFakeUsers() map[string]string {
+	userInfo := make(map[string]string)
+	userInfo["user_name"] = fake.UserName()
+	userInfo["passwd"] = fake.Password(8, 16, true, true, true)
+	userInfo["title"] = fake.Title()
+	userInfo["user_agent"] = fake.UserAgent()
+	userInfo["gender"] = fake.Gender()
+	userInfo["email"] = fake.EmailAddress()
+	userInfo["company"] = fake.Company()
+	userInfo["country"] = fake.Country()
+	userInfo["job_title"] = fake.JobTitle()
+	userInfo["ipv4"] = fake.IPv4()
+	userInfo["ipv6"] = fake.IPv6()
+	userInfo["addrees"] = fake.Continent() + "-" + fake.Country() + "-" + fake.State() + "-" + fake.City() + "-" + fake.StreetAddress()
+
+	return userInfo
+
+}
+
+func GenFakeEmails() map[string]string {
+	fakeEmail := make(map[string]string)
+	fakeEmail ["email_address"] = fake.EmailAddress()
+	fakeEmail ["email_body"] = fake.EmailBody()
+	fakeEmail ["email_subject"] = fake.EmailSubject()
+
+	return fakeEmail
+
+}
+
+func GenFakeIps() map[string]string {
+	fakeIps := make(map[string]string)
+	fakeIps["ipv4"] = fake.IPv4()
+	fakeIps["ipv6"] = fake.IPv6()
+	fakeIps["domain_name"] = fake.DomainName()
+	fakeIps["domain_zone"] = fake.DomainZone()
+	return fakeIps
+
+}
+
+func GenFakeLocs() map[string]string {
+	locationInfo := make(map[string]string)
+	locationInfo["cpuntry"] = fake.Country()
+	locationInfo["city"] = fake.City()
+	locationInfo["street_address"] = fake.StreetAddress()
+	locationInfo["street"] = fake.Street()
+
+	locationInfo["latitude"] = fmt.Sprintf("%f", fake.Latitude())
+	locationInfo["latitude_degrees"] = fmt.Sprintf("%d", fake.LatitudeDegrees())
+	locationInfo["latitude_direction"] = fake.LatitudeDirection()
+	locationInfo["latitude_minutes"] = fmt.Sprintf("%d", fake.LatitudeMinutes())
+	locationInfo["latitude_seconds"] = strconv.Itoa( fake.LatitudeSeconds())
+	locationInfo["longitude_drees"] = fmt.Sprintf("%d", fake.LongitudeDegrees())
+	locationInfo["longitude"] = fmt.Sprintf("%f", fake.Longitude())
+	locationInfo["longitude_direction"] = fake.LongitudeDirection()
+	locationInfo["longitude_minutes"] = fmt.Sprintf("%d", fake.LongitudeMinutes())
+	locationInfo["longitude_second"] = fmt.Sprintf("%d", fake.LongitudeSeconds())
+
+	return locationInfo
+
+}
+func GenFakeCreditCard() map[string]string {
+	fakeCredit := make(map[string]string)
+	fakeCredit["credit_type"] = fake.CreditCardType()
+	fakeCredit["credit_num"] = fake.CreditCardNum("")
+	return fakeCredit
+
+}
+func GenFakeTimes() map[string]string {
+	timeInfo := make(map[string]string)
+	timeInfo["day"] = strconv.Itoa(fake.Day())
+	timeInfo["weekday"] = fake.WeekDay()
+	timeInfo["weekdaynum"] = strconv.Itoa(fake.WeekdayNum())
+	timeInfo["dayshort"] = fake.WeekDayShort()
+	timeInfo["month"] = fake.Month()
+	timeInfo["month_num"] = strconv.Itoa(fake.MonthNum())
+	timeInfo["month_short"] = fake.MonthShort()
+	timeInfo["year"] = strconv.Itoa(fake.Year(0, time.Now().Year()))
+	timeInfo["now"] = time.Now().String()
+	timeInfo["now_unix"] = strconv.FormatInt(time.Now().Unix(), 10)
+	return timeInfo
+
+}
+
+
+
+
+
--- web/model/resources/resources_category.go
+++ web/model/resources/resources_category.go
@@ -7,11 +7,11 @@ type ResourceBlogs struct {
 
 	Name        string    `json:"name" gorm:"column:name;type:varchar(128);"`
 	Author      string    `json:"author" gorm:"column:author;type:varchar(128);"`
-	Category    string    `json:"category" gorm:"column:doc_category;type:varchar(128);"`
+	Category    string    `json:"category" gorm:"column:category;type:varchar(128);"`
 	Tags        string    `json:"tags" gorm:"column:tags;type:varchar(128);"`
 	ContentMd   string    `json:"content_md" gorm:"column:content_md;type:text;"`
 	ContentHtml string    `json:"content_html" gorm:"column:content_html;type:text;"`
-	Link        string    `json:"link" gorm:"column:link;type:varchar(128);"`
+	Link        string    `json:"link" gorm:"column:links;type:varchar(128);"`
 	CreatedAt   int64 `json:"created_at" gorm:"column:created_at;type:bigint(20);"`
 	UpdatedAt   int64 `json:"updated_at" gorm:"column:updated_at;type:bigint(20);"`
 	DeletedAt   int64 `json:"deleted_at" gorm:"column:deleted_at;type:bigint(20);"`
