28b2da25b048f1bb3cca68d0d416710ef5b51684
duliang <liang.du@nscc-gz.cn>
2020-12-24 10:06:30 +0800

    add blogs

--- website/src/views/resources/blogs/list.vue
+++ website/src/views/resources/blogs/list.vue
@@ -83,7 +83,44 @@
         </div>
       </section>
       <section>
+        <div class="container">
+          <div class="notification is-primary">
+            This container is <strong>centered</strong> on desktop and larger viewports.
 
+          </div>
+          <div v-for="blog in blogs">
+            <div class="card ">
+              <div class="card-image">
+                <figure class="image">
+                  <img src="../../../assets/images/resources/blogs/background_blogs.jpg" alt="Placeholder image" height="20" width="20">
+                </figure>
+                {{blog}}
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">John Smith</p>
+                    <p class="subtitle is-6">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                  <br>
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+            <div>&nbsp;&nbsp;<br></br></div>
+          </div>
+        </div>
         <div class="container">
           <div class="notification is-primary">
             This container is <strong>centered</strong> on desktop and larger viewports.
