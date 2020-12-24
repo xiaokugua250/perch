d618710848bde0d8692ac2359a7b81a018327d9b
duliang <liang.du@nscc-gz.cn>
2020-12-24 10:07:36 +0800

    add blogs

--- website/src/views/resources/blogs/list.vue
+++ website/src/views/resources/blogs/list.vue
@@ -94,7 +94,7 @@
                 <figure class="image">
                   <img src="../../../assets/images/resources/blogs/background_blogs.jpg" alt="Placeholder image" height="20" width="20">
                 </figure>
-                {{blog}}
+                {{ blog }}
               </div>
               <div class="card-content">
                 <div class="media">
@@ -244,15 +244,7 @@
         </div>
 
       </section>
-      <footer class="footer">
-        <div class="content has-text-centered">
-          <p>
-            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-          </p>
-        </div>
-      </footer>
+
 
     </body>
   </div>
