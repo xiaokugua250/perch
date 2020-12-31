e21d25fea044a1f109b1be322ee750683bd19679
liangdu <dud250858386>
2020-12-30 18:12:44 -0800

    fix code

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,14 +1,14 @@
 <template>
 
   <div id="app">
-      <head>
-             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <head>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
       <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
       <!-- Bulma Version 0.9.0-->
       <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.0/css/bulma.min.css">
       <link rel="stylesheet" type="text/css" href="../css/hero.css">
       <link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css">
-      </head>
+    </head>
     <router-view />
     <script type="application/ld+json">
       {
--- website/src/views/sites/index.vue
+++ website/src/views/sites/index.vue
@@ -9,7 +9,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>平台主页</title>
-    
+
     </head>
     <body>
       <!-- Google Tag Manager (noscript) -->
