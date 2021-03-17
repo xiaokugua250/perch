b24ea5c6c204899ba65ae77cf52d8cd62f23fce2
xiaokugua250 <xiaokugua250@hotmail.com>
2021-03-17 09:18:14 +0800

    add code

--- pkg/proxy/proxy_basic.go
+++ pkg/proxy/proxy_basic.go
@@ -105,9 +105,30 @@ func ProxyServerSetup()(error){
 	}()
 	
 	go func(){
-		 http
+		
+		
+		 
 	}()
 
+ 	 closeSignal := make(chan os.Signal, 1)
+    // We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+    // SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+    signal.Notify(closeSignal, os.Interrupt)
+
+    // Block until we receive our signal.
+    <-closeSignal
+
+    // Create a deadline to wait for.
+    ctx, cancel := context.WithTimeout(context.Background(), wait)
+    defer cancel()
+    // Doesn't block if no connections, but will otherwise wait
+    // until the timeout deadline.
+    httpServer.Shutdown(ctx)
+    // Optionally, you could run srv.Shutdown in a goroutine and block on
+    // <-ctx.Done() if your application should wait for other services
+    // to finalize based on context cancellation.
+    log.Println("shutting down")
+    os.Exit(0)
 }
 
 
