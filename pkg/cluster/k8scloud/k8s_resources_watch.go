b7bdda7c9bd4e54404aabab4f97d9ebfa8a3259f
duliang <liang.du@nscc-gz.cn>
2021-03-31 17:44:15 +0800

    add code

--- pkg/cluster/k8scloud/k8s_resources_watch.go
+++ pkg/cluster/k8scloud/k8s_resources_watch.go
@@ -1,5 +1,41 @@
+/**
+采用watch机制获取集群信息
+ */
 package k8scloud
 
 import (
 	_ "perch/pkg/log"
 )
+
+
+/**
+获取集群概览信息
+ */
+func (k8sClient *clusterClientManager)ClusterOverview(){
+
+}
+
+
+/**
+获取节点概览信息
+ */
+func (k8sClient *clusterClientManager)ClusterNodeOverview()  {
+
+}
+
+/**
+获取pod概览信息
+ */
+func (k8sClient *clusterClientManager)ClutserPodOverview(){
+
+}
+
+
+
+
+/**
+获取资源概览信息
+ */
+func (k8sClient *clusterClientManager)ClusterResourceOverview(){
+
+}
--- pkg/cluster/k8scloud/k8s_submit.go
+++ pkg/cluster/k8scloud/k8s_submit.go
@@ -2,17 +2,15 @@ package k8scloud
 
 import (
 	"context"
-	"github.com/coreos/etcd/etcdserver/api"
-	"github.com/coreos/etcd/etcdserver/auth"
 	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/api/batch/v1beta1"
 	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	"k8s.io/client-go/util/retry"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"log"
 	model "perch/web/model/rbac"
-	"strconv"
 )
 
 /**
@@ -59,8 +57,14 @@ func (k8sClient *clusterClientManager) SubmitDeploymentType(ns string, dName str
 
 							},
 							Resources: apiv1.ResourceRequirements{
-								Requests: requests,
-								Limits:   requests,
+								Requests: apiv1.ResourceList{
+									"cpu":resource.MustParse("cpuRequest"),
+									"memory": resource.MustParse("memoryRequest"),
+								},
+								Limits:   apiv1.ResourceList{
+									"cpu":resource.MustParse("cpuLimit"),
+									"memory": resource.MustParse("memoryLimit"),
+								},
 							},
 							VolumeMounts: []apiv1.VolumeMount{
 
@@ -113,22 +117,237 @@ func (k8sClient *clusterClientManager) SubmitDeploymentType(ns string, dName str
 /**
 创建网络Service类型
 */
-func SubmitServiceType() {
+func  (k8sClient *clusterClientManager)SubmitServiceType(ns string, serviceName string, lables map[string]string)(*apiv1.Service,error) {
+	var (
+		k8sService *apiv1.Service
+		err       error
+	)
 
+	k8sService = &apiv1.Service{
+
+		ObjectMeta: metav1.ObjectMeta{
+			Name:                       serviceName,
+			Namespace:                  ns,
+			Labels: lables,
+		},
+		Spec: apiv1.ServiceSpec{
+			Ports: []apiv1.ServicePort{
+				{
+					Name:       "terminal",
+					Port:       8000,
+					TargetPort: intstr.FromInt(8000),
+				},
+				{
+					Name:       "ssh",
+					Port:       22,
+					TargetPort: intstr.FromInt(22),
+				},
+
+			},
+			Selector: lables,
+		},
+
+
+}
+
+	k8sService, err = k8sClient.clusterClient.CoreV1().Services(ns).Create(context.Background(),k8sService,metav1.CreateOptions{})
+	return k8sService, err
 }
 
 /**
 提交job类型资源
 */
-func SubmitJobType() {
+func  (k8sClient *clusterClientManager)SubmitJobType(ns string, jobName string, lables map[string]string,
+	imageName string, Image string,
+	user model.AuthUser) {
+	jobs := k8sClient.clusterClient.BatchV1().Jobs("default")
+	var backOffLimit int32 = 0
 
+	jobSpec := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      jobName,
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Template: apiv1.PodTemplateSpec{
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:            imageName,
+							Image:           Image,
+							ImagePullPolicy: "Always",
+							Command:         []string{},
+							Env: []apiv1.EnvVar{
+								{
+									Name:  "USER",
+									Value: user.UserName,
+								},
+								/*	{
+									Name:  "HOME",
+									Value: "/home/" + userName,
+								},*/
+
+							},
+							Resources: apiv1.ResourceRequirements{
+								Requests: apiv1.ResourceList{
+									"cpu":resource.MustParse("cpuRequest"),
+									"memory": resource.MustParse("memoryRequest"),
+								},
+								Limits:   apiv1.ResourceList{
+									"cpu":resource.MustParse("cpuLimit"),
+									"memory": resource.MustParse("memoryLimit"),
+								},
+							},
+							VolumeMounts: []apiv1.VolumeMount{
+
+								{
+									Name:      "shm",
+									MountPath: "/dev/shm",
+								},
+							},
+							SecurityContext: &apiv1.SecurityContext{
+								Capabilities: &apiv1.Capabilities{
+									Add: []apiv1.Capability{
+										"SYS_ADMIN",
+									},
+								},
+							},
+						},
+					},
+					Volumes: []apiv1.Volume{
+						{
+							Name: "home",
+							VolumeSource: apiv1.VolumeSource{
+								HostPath: &apiv1.HostPathVolumeSource{
+									//	Path: "/home/" + userName,
+								},
+							},
+						},
+
+						{
+							Name: "shm",
+							VolumeSource: apiv1.VolumeSource{
+								EmptyDir: &apiv1.EmptyDirVolumeSource{
+									Medium: apiv1.StorageMediumMemory,
+								},
+							},
+						},
+					},
+					SecurityContext: &apiv1.PodSecurityContext{
+						RunAsUser:  &user.UserUID,
+						RunAsGroup: &user.UserGID,
+					},
+					RestartPolicy: apiv1.RestartPolicyNever,
+				},
+			},
+			BackoffLimit: &backOffLimit,
+		},
+	}
+
+	_, err := jobs.Create(context.TODO(), jobSpec, metav1.CreateOptions{})
+	if err != nil {
+		log.Fatalln("Failed to create K8s job.")
+	}
+
+	//print job details
+	log.Println("Created K8s job successfully")
 }
 
 /**
 提交cronjob类型资源
 */
-func SubmitCronJobType() {
+func  (k8sClient *clusterClientManager)SubmitCronJobType(ns string, jobName string, lables map[string]string,
+	imageName string, Image string,
+	user model.AuthUser) {
 
+	cronjob := &v1beta1.CronJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      jobName,
+			Namespace: ns,
+		},
+		Spec: v1beta1.CronJobSpec{
+			Schedule:          "* * * * *",
+			ConcurrencyPolicy: v1beta1.ForbidConcurrent,
+			JobTemplate: v1beta1.JobTemplateSpec{
+				Spec: batchv1.JobSpec{
+					Template: apiv1.PodTemplateSpec{
+						Spec: apiv1.PodSpec{
+							Containers: []apiv1.Container{
+								{
+									Name:            imageName,
+									Image:           Image,
+									ImagePullPolicy: "Always",
+									Command:         []string{},
+									Env: []apiv1.EnvVar{
+										{
+											Name:  "USER",
+											Value: user.UserName,
+										},
+										/*	{
+											Name:  "HOME",
+											Value: "/home/" + userName,
+										},*/
+
+									},
+									Resources: apiv1.ResourceRequirements{
+										Requests: apiv1.ResourceList{
+											"cpu":resource.MustParse("cpuRequest"),
+											"memory": resource.MustParse("memoryRequest"),
+										},
+										Limits:   apiv1.ResourceList{
+											"cpu":resource.MustParse("cpuLimit"),
+											"memory": resource.MustParse("memoryLimit"),
+										},
+									},
+									VolumeMounts: []apiv1.VolumeMount{
+
+										{
+											Name:      "shm",
+											MountPath: "/dev/shm",
+										},
+									},
+									SecurityContext: &apiv1.SecurityContext{
+										Capabilities: &apiv1.Capabilities{
+											Add: []apiv1.Capability{
+												"SYS_ADMIN",
+											},
+										},
+									},
+								},
+							},
+							Volumes: []apiv1.Volume{
+								{
+									Name: "home",
+									VolumeSource: apiv1.VolumeSource{
+										HostPath: &apiv1.HostPathVolumeSource{
+											//	Path: "/home/" + userName,
+										},
+									},
+								},
+
+								{
+									Name: "shm",
+									VolumeSource: apiv1.VolumeSource{
+										EmptyDir: &apiv1.EmptyDirVolumeSource{
+											Medium: apiv1.StorageMediumMemory,
+										},
+									},
+								},
+							},
+							SecurityContext: &apiv1.PodSecurityContext{
+								RunAsUser:  &user.UserUID,
+								RunAsGroup: &user.UserGID,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err := k8sClient.clusterClient.BatchV1beta1().CronJobs("ns").Create(context.TODO(), cronjob, metav1.CreateOptions{})
+	if err != nil {
+		log.Fatalln("Failed to create K8s job.")
+	}
 }
 
 func int32Ptr(i int32) *int32 { return &i }
