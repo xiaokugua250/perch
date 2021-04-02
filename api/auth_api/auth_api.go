50b289746b48ab8a5874ae70867c7ecdeff85784
duliang <liang.du@nscc-gz.cn>
2021-04-02 11:54:29 +0800

    1.添加casbin rule 后台api

--- api/auth_api/auth_api.go
+++ api/auth_api/auth_api.go
@@ -1,1 +1,155 @@
 package auth_api
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	database "perch/database/mysql"
+	casbin "perch/pkg/auth/casbin"
+	"perch/web/metric"
+	"perch/web/model"
+
+	"strconv"
+)
+
+func CasbinAuthSpcGetHandler(w http.ResponseWriter, r *http.Request) {
+	metric.ProcessMetricFunc(w, r, nil, &metric.MiddlewarePlugins{}, func(ctx context.Context, bean interface{}, response *model.ResultResponse) error {
+		var (
+			userCasbinsRules casbin.CasbinRule
+			id               int
+			err              error
+		)
+		response.Kind = "casbin auths"
+		id, err = strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+		if err = database.MysqlDb.Where("id=?", id).First(userCasbinsRules).Error; err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+
+		response.Code = http.StatusOK
+		response.Spec = userCasbinsRules
+		response.Message = " get auth user casbin rules successfully"
+		return nil
+	})
+}
+
+func CasbinAuthGetHandler(w http.ResponseWriter, r *http.Request) {
+	metric.ProcessMetricFunc(w, r, nil, &metric.MiddlewarePlugins{}, func(ctx context.Context, bean interface{}, response *model.ResultResponse) error {
+		var (
+			userCasbinsRules []casbin.CasbinRule
+
+			err error
+		)
+		response.Kind = "casbin auths"
+
+		if err = database.MysqlDb.Find(&userCasbinsRules).Error; err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+
+		if err = database.MysqlDb.Model(&casbin.CasbinRule{}).Count(&response.Total).Error; err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+		response.Code = http.StatusOK
+		response.Spec = userCasbinsRules
+		response.Message = " get auth users casbin rules successfully"
+		return nil
+	})
+}
+
+func CasbinAuthCreateHandler(w http.ResponseWriter, r *http.Request) {
+	metric.ProcessMetricFunc(w, r, nil, &metric.MiddlewarePlugins{}, func(ctx context.Context, bean interface{}, response *model.ResultResponse) error {
+		var (
+			userCasbinsRule casbin.CasbinRule
+
+			err error
+		)
+		response.Kind = "casbin auths"
+
+		if err = json.NewDecoder(r.Body).Decode(&userCasbinsRule); err != nil {
+			response.Code = http.StatusBadRequest
+			response.Message = err.Error()
+			return err
+		}
+		if err = database.MysqlDb.Create(userCasbinsRule).Error; err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+
+		response.Code = http.StatusOK
+		response.Spec = userCasbinsRule
+		response.Message = " get auth users successfully"
+		return nil
+	})
+}
+
+func CasbinAuthPatchHandler(w http.ResponseWriter, r *http.Request) {
+	metric.ProcessMetricFunc(w, r, nil, &metric.MiddlewarePlugins{}, func(ctx context.Context, bean interface{}, response *model.ResultResponse) error {
+		var (
+			userCasbinsRule casbin.CasbinRule
+			id              int
+			err             error
+		)
+		response.Kind = "casbin auths"
+		id, err = strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+		if err = json.NewDecoder(r.Body).Decode(&userCasbinsRule); err != nil {
+			response.Code = http.StatusBadRequest
+			response.Message = err.Error()
+			return err
+		}
+		if err = database.MysqlDb.Where("id=?", id).Model(casbin.CasbinRule{}).Updates(userCasbinsRule).Error; err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+
+		response.Code = http.StatusOK
+		response.Spec = userCasbinsRule
+		response.Message = "update auth users casbin successfully"
+		return nil
+	})
+}
+
+func CasbinAuthDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	metric.ProcessMetricFunc(w, r, nil, &metric.MiddlewarePlugins{}, func(ctx context.Context, bean interface{}, response *model.ResultResponse) error {
+		var (
+			userCasbinsRule casbin.CasbinRule
+			id              int
+			err             error
+		)
+		response.Kind = "casbin auths"
+		id, err = strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+
+		if err = database.MysqlDb.Where("id=?", id).Delete(&userCasbinsRule).Error; err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Message = err.Error()
+			return err
+		}
+
+		response.Code = http.StatusOK
+		response.Spec = userCasbinsRule
+		response.Message = " delete user casbin role successfully"
+		return nil
+	})
+}
--- web/server/sys_server/sys_server.go
+++ web/server/sys_server/sys_server.go
@@ -9,21 +9,21 @@ import (
 func main() {
 	//
 	serverRouter := []service.WebRouter{
-		{RouterPath: "/monitor/basicinfo", RouterHandlerFunc: sysadmin.SysBasicInfoHandler, RouterMethod: http.MethodGet},
-		{RouterPath: "/monitor/memadvanced", RouterHandlerFunc: sysadmin.SysMemInfoHandler, RouterMethod: http.MethodGet},
-		{RouterPath: "/monitor/hostadvanced", RouterHandlerFunc: sysadmin.SysHostInfoHandler, RouterMethod: http.MethodGet},
+		{RouterPath: "/basicinfo", RouterHandlerFunc: sysadmin.SysBasicInfoHandler, RouterMethod: http.MethodGet},
+		{RouterPath: "/memadvanced", RouterHandlerFunc: sysadmin.SysMemInfoHandler, RouterMethod: http.MethodGet},
+		{RouterPath: "/hostadvanced", RouterHandlerFunc: sysadmin.SysHostInfoHandler, RouterMethod: http.MethodGet},
 
-		{RouterPath: "/monitor/cpuadvanced", RouterHandlerFunc: sysadmin.SysCpuInfoHandler, RouterMethod: http.MethodGet},
+		{RouterPath: "/cpuadvanced", RouterHandlerFunc: sysadmin.SysCpuInfoHandler, RouterMethod: http.MethodGet},
 
-		{RouterPath: "/monitor/dockeradvanced", RouterHandlerFunc: sysadmin.SysDockerInfoHandler, RouterMethod: http.MethodGet},
+		{RouterPath: "/dockeradvanced", RouterHandlerFunc: sysadmin.SysDockerInfoHandler, RouterMethod: http.MethodGet},
 
-		{RouterPath: "/monitor/diskadvanced", RouterHandlerFunc: sysadmin.SysDiskInfoHandler, RouterMethod: http.MethodGet},
+		{RouterPath: "/diskadvanced", RouterHandlerFunc: sysadmin.SysDiskInfoHandler, RouterMethod: http.MethodGet},
 
-		{RouterPath: "/monitor/loadadvanced", RouterHandlerFunc: sysadmin.SysLoadInfoHandler, RouterMethod: http.MethodGet},
+		{RouterPath: "/loadadvanced", RouterHandlerFunc: sysadmin.SysLoadInfoHandler, RouterMethod: http.MethodGet},
 
-		{RouterPath: "/monitor/netadvanced", RouterHandlerFunc: sysadmin.SysNetInfoHandler, RouterMethod: http.MethodGet}, //todo 方法需完善
+		{RouterPath: "/netadvanced", RouterHandlerFunc: sysadmin.SysNetInfoHandler, RouterMethod: http.MethodGet}, //todo 方法需完善
 
-		{RouterPath: "/monitor/processadvanced", RouterHandlerFunc: sysadmin.SysProcessInfoHandler, RouterMethod: http.MethodGet}, //todo 方法需完善
+		{RouterPath: "/processadvanced", RouterHandlerFunc: sysadmin.SysProcessInfoHandler, RouterMethod: http.MethodGet}, //todo 方法需完善
 
 	}
 	webServer := service.NewWebServerWithOptions("sys-micro")
